ada844c51146b86a4b1b03bf781ca0c9d4b34fa8
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-04-18 23:05:47 -0300

    Add files via upload

--- Desafio#1_CursoR.R
+++ Desafio#1_CursoR.R
@@ -4,13 +4,56 @@
 # Autoria do script: Jeanne Franco ------------------------------------------------------------------------------------------------------
 # Data: 16/04/2022 ----------------------------------------------------------------------------------------------------------------------
 
+# Pacote necessário ------------------------------------------------------------------------------------------------------------------------
+
+library(tidyverse)
+
 # Desafio:  --------------------------------------------------------------------------------------------------------------------------------
 
 ## Escreva um código que transforme a base do IMDB em uma base nomeada IMDB_atores, que deve ser 
 ## formada pelas seguintes colunas:
 
-### ator: nome de um(a) ator/atriz (cada nome deve aparecer uma vez só)
+#### ator: nome de um(a) ator/atriz (cada nome deve aparecer uma vez só)
 
 base <- readRDS("imdb.rds")
 View(base)
 
+base %>% 
+  select(starts_with("ator")) %>% 
+  head(3)
+
+base %>% 
+  pivot_longer(
+    cols = starts_with("ator"), 
+    names_to = "protagonismo",
+    values_to = "ator_atriz"
+  ) %>% 
+  select(ator_atriz) %>% 
+  head(6)
+
+#### nota_media_imdb: nota imdb média dos filmes que o(a) ator/atriz participou
+
+base %>% 
+  pivot_longer(
+    cols = starts_with("ator"), 
+    names_to = "protagonismo",
+    values_to = "ator_atriz"
+  ) %>% 
+  select(ator_atriz, nota_imdb) %>%
+  group_by(ator_atriz) %>%
+  summarise(nota_media_imdb = mean(nota_imdb, na.rm = TRUE))
+
+
+#### media_lucro: lucro médio dos filmes que o(a) ator/atriz participou
+
+base %>% 
+  pivot_longer(
+    cols = starts_with("ator"), 
+    names_to = "protagonismo",
+    values_to = "ator_atriz"
+  ) %>% 
+  select(ator_atriz, nota_imdb, receita, orcamento) %>%
+  mutate(lucro = receita - orcamento) %>%
+  group_by(ator_atriz) %>%
+  summarise(media_lucro = mean(lucro, na.rm = TRUE),
+            nota_media_imdb = mean(nota_imdb, na.rm = TRUE))
